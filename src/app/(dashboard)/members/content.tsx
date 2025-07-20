a58e24fd14fee2adcbaf9ca864155ba36f19cec9
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-07-20 21:01:56 +0900

    APi経由で取得

--- src/app/(dashboard)/members/content.tsx
+++ src/app/(dashboard)/members/content.tsx
@@ -1,11 +1,17 @@
-import { PrismaClient } from "@/generated/prisma";
 import { use } from "react";
 import DataGridDemo from "./datagrid";
 import { Stack } from "@mui/material";
+import { ListResponse } from "@/types/rest";
+import { User } from "@/types/user";
 
 export default function MembersContents() {
-  const prisma = new PrismaClient();
-  const members = use(prisma.members.findMany());
+  const fetchUsers = async () => {
+    const response = (await (
+      await fetch(`${process.env.API_URL}/v1/users`)
+    ).json()) as ListResponse<User>;
+    return response.data;
+  };
+  const members = use(fetchUsers());
 
   const rows = members.map((member) => ({
     custom_id: member.custom_id,
---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+export type User = {
+  id: string;
+  custom_id: string;
+  name: string;
+  email: string;
+  external_email: string;
+  created_at: Date;
+  updated_at: Date;
+  joined_at: Date;
+  period: string;
+  is_enabled: boolean;
+};
