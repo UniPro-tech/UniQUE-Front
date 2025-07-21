5b38e86240179ebebde1a8662ec5cc1c8802c803
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-07-21 13:52:53 +0900

    プログレスの挙動を変更

--- src/app/(dashboard)/members/page.tsx
+++ src/app/(dashboard)/members/page.tsx
@@ -1,9 +1,21 @@
 import { Suspense } from "react";
 import MembersContents from "./content";
+import { LinearProgress, Stack } from "@mui/material";
 
 export default function MembersPage() {
   return (
-    <Suspense fallback={<div>Loading members...</div>}>
+    <Suspense
+      fallback={
+        <Stack
+          justifyContent={"center"}
+          alignItems="center"
+          minHeight={"100%"}
+          minWidth={"100vw"}
+        >
+          <LinearProgress style={{ width: "60%" }} />
+        </Stack>
+      }
+    >
       <MembersContents />
     </Suspense>
   );
