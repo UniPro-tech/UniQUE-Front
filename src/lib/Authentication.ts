2a4ced61fd31232d42aa72ff1ca9c34b65b4f078
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-11-06 08:32:46 +0900

    認証リクエストライブラリの定義

---
+++ src/lib/Authentication.ts
@@ -0,0 +1,53 @@
+import {
+  BadRequestError,
+  AuthenticationError,
+  AuthorizeError,
+  ServerError,
+} from "./RequestErrors";
+
+export type Credentials = {
+  username: string;
+  password: string;
+  ip: string;
+  user_agent: string;
+  remember_me?: boolean;
+  expires_at?: Date;
+  max_age?: number;
+};
+
+export interface AuthResponse {
+  session_id: string;
+  expires: string;
+  max_age: number;
+}
+
+export const authenticationRequest = async (
+  credentials: Credentials
+): Promise<AuthResponse> => {
+  console.log("Authentication request with credentials:", credentials);
+  const response = await fetch(`${process.env.AUTH_API_URL}/authentication`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(credentials),
+    credentials: "include",
+  });
+  if (!response.ok) {
+    switch (response.status) {
+      case 400:
+        throw BadRequestError;
+      case 401:
+        throw AuthenticationError;
+      case 403:
+        throw AuthorizeError;
+      case 500:
+        throw ServerError;
+      default:
+        throw ServerError;
+    }
+  } else {
+    const data: AuthResponse = await response.json();
+    return data;
+  }
+};
