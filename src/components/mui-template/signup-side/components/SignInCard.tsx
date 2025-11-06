94442130b38b9bc3cfa012a657c1c8cda4ca5ac3
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-11-06 10:27:24 +0900

    セッションがある場合はdashboardへリダイレクト

--- src/components/mui-template/signup-side/components/SignInCard.tsx
+++ src/components/mui-template/signup-side/components/SignInCard.tsx
@@ -1,7 +1,14 @@
+"use server";
 import { generateCSRFToken } from "@/lib/CSRF";
 import SignInCardClient from "./SignInCardClient";
+import { getSession } from "@/lib/Session";
+import { redirect, RedirectType } from "next/navigation";
 
-export default function SignInCard(props: { signUp?: boolean }) {
+export default async function SignInCard(props: { signUp?: boolean }) {
+  const session = await getSession();
+  if (session) {
+    redirect("/dashboard", RedirectType.replace);
+  }
   const csrfToken = generateCSRFToken("expired_" + Date.now().toString());
   return <SignInCardClient signUp={props.signUp} csrfToken={csrfToken} />;
 }
