a2649699f1effe8f6dd514fae36555f0393e0962
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-11-15 08:00:03 +0900

    所属期のMenuItemsのコンポーネントを作成

---
+++ src/lib/PeriodSelectorOptions.tsx
@@ -0,0 +1,62 @@
+import { MenuItem, Typography } from "@mui/material";
+
+/**
+ * ## PeriodSelectorOptions
+ *　所属期の選択肢を生成するコンポーネント
+ * @returns React.JSX 所属期のMenuItem
+ */
+export default function PeriodSelectorOptions() {
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+  const startYear = 2024; // サークル開始年を設定
+  const periods = ["0"];
+  const quoter = [
+    {
+      month: 4,
+      name: "A",
+    },
+    {
+      month: 7,
+      name: "B",
+    },
+    {
+      month: 10,
+      name: "C",
+    },
+    {
+      month: 1,
+      name: "D",
+    },
+  ];
+  for (let year = startYear; year <= currentYear; year++) {
+    if (year === startYear && currentMonth <= 3) continue; // 0期はすでに追加されているのでスキップ
+    for (const q of quoter) {
+      if (
+        year === currentYear &&
+        (currentMonth < q.month || (q.month === 1 && currentMonth >= 4))
+      )
+        break; // 今年度でまだ来ていない期はスキップ
+      const period = `${year - startYear + 1}${q.name}`;
+      periods.push(period);
+    }
+  }
+
+  return (
+    <>
+      {periods.map((period) => (
+        <MenuItem key={period} value={period}>
+          <Typography>{period}期</Typography>
+          <Typography variant="caption" sx={{ ml: 1, color: "text.secondary" }}>
+            ({startYear + parseInt(period) - 1}年度{" "}
+            {period.slice(0) == "0"
+              ? "早期活動者"
+              : `${
+                  quoter.find((q) => q.name === period.slice(1))?.month
+                }月以降`}
+            )
+          </Typography>
+        </MenuItem>
+      ))}
+    </>
+  );
+}
