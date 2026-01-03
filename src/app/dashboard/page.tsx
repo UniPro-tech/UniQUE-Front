274e4a3ff19e6b10b06830abac9ce5326f87a81b
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-01-04 01:49:59 +0900

    ホームを彩る

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,45 @@
-import { Stack, Typography } from "@mui/material";
+import { Card, Stack, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default async function DashboardPage() {
   return (
     <Stack>
       <Typography variant="h4" gutterBottom>
-        Dashboard
+        おかえりなさい！
       </Typography>
+      <Typography variant="body1">
+        ダッシュボードへようこそ。左のサイドバーからナビゲートしてください。
+      </Typography>
+      <Stack mt={4} spacing={2} id="announce">
+        <Typography variant="h5">お知らせ</Typography>
+        <Stack spacing={1}>
+          <Card variant="outlined" sx={{ padding: 2 }}>
+            <Typography variant="h6">🚧 Close Alpha版公開のお知らせ</Typography>
+            <Typography variant="body1">
+              この度、Close Alpha版を公開いたしました。
+              <br />
+              様々なバグ等が発生する可能性があります。
+              ご理解のほどよろしくお願いいたします。
+              <br />
+              バグが発生した場合は、下記リンクから報告をお願いいたします。
+              <br />
+              <Link
+                href="https://canary.discord.com/channels/1191346186880286770/1239215629962182789"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                バグ報告フォーム
+              </Link>
+            </Typography>
+          </Card>
+          <Card variant="outlined" sx={{ padding: 2 }}>
+            <Typography variant="h6">🛠️ サービス停止のお知らせ</Typography>
+            <Typography variant="body1">
+              2025年1月10日午後20時から午後25時までサービスが一時的に停止します。
+            </Typography>
+          </Card>
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
